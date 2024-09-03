4b2b73ddb537cb182f7d8ddf8a663c84e2128d15
Camilla Barros <camillaerbarros@gmail.com>
2024-09-03 11:05:23 -0300

    feat(ui): contando número de likes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,61 @@
+// follow
+var isFollowed = false;
+function follow() {
+    var button = document.getElementById("btn-follow");
+    var icon = button === null || button === void 0 ? void 0 : button.children[0];
+    if (!icon)
+        return;
+    if (isFollowed) {
+        icon.classList.remove("followed-style");
+        icon.innerHTML = "Follow";
+    }
+    else {
+        icon.classList.add("followed-style");
+        icon.innerHTML = "Following";
+    }
+    isFollowed = !isFollowed;
+}
 // like
 var isLiked = false;
+var likeCount = 0;
 function like() {
     // to get the element by id
     var button = document.getElementById("btn-like");
     var icon = button === null || button === void 0 ? void 0 : button.children[0];
-    //   console.log(icon);
+    var likeCountElement = document.getElementById("like-count");
     // make a condition to verify if icon isn't undefined or null
-    if (!icon)
+    if (!icon || !likeCountElement)
         return;
     // to remove the filled heart and add an empty heart
     if (isLiked) {
         icon.classList.remove("fa-heart");
         icon.classList.remove("liked");
         icon.classList.add("fa-heart-o");
+        likeCount--;
     }
     else {
         // to add an filled heart and remove the empty heart
         icon.classList.add("fa-heart");
         icon.classList.add("liked");
         icon.classList.remove("fa-heart-o");
+        likeCount++;
     }
+    likeCountElement.innerText = likeCount.toString();
     isLiked = !isLiked;
 }
 // bookmark
 var isSaved = false;
 function save() {
     var button = document.getElementById("btn-save");
-    // console.log("botão: ", button);
     var icon = button === null || button === void 0 ? void 0 : button.children[0];
-    // console.log("filho: ", icon);
     if (!icon)
         return;
     if (isSaved) {
-        console.log("entrou no if");
         icon.classList.remove("fa-bookmark");
         icon.classList.remove("saved");
         icon.classList.add("fa-bookmark-o");
     }
     else {
-        console.log("entrou no else");
         icon.classList.add("fa-bookmark");
         icon.classList.add("saved");
         icon.classList.remove("fa-bookmark-o");
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,56 @@
+// follow
+
+let isFollowed = false;
+
+function follow() {
+  const button = document.getElementById("btn-follow");
+  const icon = button?.children[0];
+
+  if (!icon) return;
+
+  if (isFollowed) {
+    icon.classList.remove("followed-style");
+    icon.innerHTML = "Follow";
+  } else {
+    icon.classList.add("followed-style");
+    icon.innerHTML = "Following";
+  }
+
+  isFollowed = !isFollowed;
+}
+
 // like
 
 let isLiked = false;
+let likeCount = 0;
 
 function like() {
   // to get the element by id
   const button = document.getElementById("btn-like");
   const icon = button?.children[0];
-  //   console.log(icon);
+
+  let likeCountElement = document.getElementById("like-count");
 
   // make a condition to verify if icon isn't undefined or null
-  if (!icon) return;
+  if (!icon || !likeCountElement) return;
 
   // to remove the filled heart and add an empty heart
   if (isLiked) {
     icon.classList.remove("fa-heart");
     icon.classList.remove("liked");
     icon.classList.add("fa-heart-o");
+
+    likeCount--;
   } else {
     // to add an filled heart and remove the empty heart
     icon.classList.add("fa-heart");
     icon.classList.add("liked");
     icon.classList.remove("fa-heart-o");
+
+    likeCount++;
   }
+
+  likeCountElement.innerText = likeCount.toString();
 
   isLiked = !isLiked;
 }
@@ -32,19 +61,15 @@ let isSaved = false;
 
 function save() {
   const button = document.getElementById("btn-save");
-  // console.log("botão: ", button);
   const icon = button?.children[0];
-  // console.log("filho: ", icon);
 
   if (!icon) return;
 
   if (isSaved) {
-    console.log("entrou no if");
     icon.classList.remove("fa-bookmark");
     icon.classList.remove("saved");
     icon.classList.add("fa-bookmark-o");
   } else {
-    console.log("entrou no else");
     icon.classList.add("fa-bookmark");
     icon.classList.add("saved");
     icon.classList.remove("fa-bookmark-o");
